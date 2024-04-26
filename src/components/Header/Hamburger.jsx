2424f677f0a9f334b210b1c890f99fa6e410a752
KevinZheng99 <kevinzhengkz99@gmail.com>
2024-04-26 15:27:20 -0400

    socials side bar

--- src/components/Header/Hamburger.jsx
+++ src/components/Header/Hamburger.jsx
@@ -1,9 +1,9 @@
 function Hamburger() {
   return (
     <button className="mr-16">
-      <div className="border-black border-b-4 w-7 mb-1" />
-      <div className="border-black border-b-4 w-7 mb-1" />
-      <div className="border-black border-b-4 w-7 mb-1" />
+      <div className="border-slate-200 border-b-4 w-7 mb-1" />
+      <div className="border-slate-200 border-b-4 w-7 mb-1" />
+      <div className="border-slate-200 border-b-4 w-7 mb-1" />
     </button>
   );
 }
--- src/components/Header/Socials.jsx
+++ src/components/Header/Socials.jsx
@@ -1,5 +1,39 @@
+import Discord from "../Icons/Discord";
+import Facebook from "../Icons/Facebook";
+import Instagram from "../Icons/Instagram";
+import TikTok from "../Icons/TikTok";
+import Twitter from "../Icons/Twitter";
+
 function Socials() {
-  return <div>Socials</div>;
+  const svgClasses = "text-slate-200";
+
+  return (
+    <div className="absolute flex flex-col items-center justify-center gap-6 top-24 right-16">
+      <a href="https://www.instagram.com/ye" target="_blank" rel="noreferrer">
+        <Instagram styles={svgClasses} />
+      </a>
+      <a href="https://twitter.com/kanyewest" target="_blank" rel="noreferrer">
+        <Twitter styles={svgClasses} />
+      </a>
+      <a
+        href="https://www.facebook.com/kanyewest"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Facebook styles={svgClasses} />
+      </a>
+      <a href="https://www.tiktok.com/@ye" target="_blank" rel="noreferrer">
+        <TikTok styles={svgClasses} />
+      </a>
+      <a
+        href="https://discord.com/invite/kanyewest"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Discord styles={svgClasses} />
+      </a>
+    </div>
+  );
 }
 
 export default Socials;
