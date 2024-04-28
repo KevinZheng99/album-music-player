1925974afaf7df11e2c4917a96c38ec190eaa929
KevinZheng99 <kevinzhengkz99@gmail.com>
2024-04-27 21:54:35 -0400

    Hero Section complete

--- src/components/Hero/AlbumContainer.jsx
+++ src/components/Hero/AlbumContainer.jsx
@@ -2,7 +2,7 @@ import albumCover from "../../assets/MBDTF.jpg";
 
 function AlbumContainer() {
   return (
-    <div className="absolute -rotate-90 top-0">
+    <div className="-rotate-90 -translate-y-20">
       <p className="text-slate-200 italic text-xl mb-2">
         <span className="text-[#66f0d4]">Def Jam Recordings</span> and{" "}
         <span className="text-[#66f0d4]">Rock-A-Fella Records</span> — Released
--- src/components/Hero/HeroContainer.jsx
+++ src/components/Hero/HeroContainer.jsx
@@ -1,9 +1,42 @@
 import AlbumContainer from "./AlbumContainer";
+import Play from "../Icons/Play";
+import Share from "../Icons/Share";
 
 function HeroContainer() {
+  const buttonStyles =
+    "flex items-center gap-2 text-slate-200 px-6 py-4 text-xl font-medium";
+  const buttonHoverStyles =
+    "hover:bg-slate-200 hover:border-slate-200 hover:text-black";
+
   return (
-    <div>
+    <div className="flex items-center mb-32">
       <AlbumContainer />
+      <div>
+        <p className=" mb-4 text-slate-200 text-7xl font-bold">MBDTF</p>
+        <p className=" mb-16 text-slate-200 text-5xl font-bold">Kanye West</p>
+        <div className="flex">
+          <button
+            className={
+              buttonStyles +
+              " bg-red-500 border-red-500 border-2 mr-4 transition-all " +
+              buttonHoverStyles
+            }
+          >
+            <Play />
+            LISTEN NOW
+          </button>
+          <button
+            className={
+              buttonStyles +
+              " border-slate-200 border-2 transition-all " +
+              buttonHoverStyles
+            }
+          >
+            <Share />
+            SHARE
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
