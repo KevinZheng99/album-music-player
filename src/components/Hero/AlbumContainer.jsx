125f97c271136299d14dc64fcd62f4432fd7ecd3
KevinZheng99 <kevinzhengkz99@gmail.com>
2024-04-29 23:26:31 -0400

    basic responsiveness

--- src/components/Hero/AlbumContainer.jsx
+++ src/components/Hero/AlbumContainer.jsx
@@ -2,13 +2,13 @@ import albumCover from "../../assets/MBDTF.jpg";
 
 function AlbumContainer() {
   return (
-    <div className="-rotate-90 w-full">
-      <p className="text-slate-200 italic text-xl xl:text-base 2xl:text-xl mb-2">
+    <div className="w-full 860px:rotate-90">
+      <img className="xl:w-full 860px:-rotate-90" src={albumCover} />
+      <p className="text-slate-200 italic text-lg 860px:text-xl xl:text-base 2xl:text-xl mb-2 860px:rotate-180">
         <span className="text-[#66f0d4]">Def Jam Recordings</span> and{" "}
         <span className="text-[#66f0d4]">Rock-A-Fella Records</span> — Released
         Nov 22, 2010
       </p>
-      <img className="xl:w-full rotate-90" src={albumCover} />
     </div>
   );
 }
