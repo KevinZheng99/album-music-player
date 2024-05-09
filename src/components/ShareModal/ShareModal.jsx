faa2aa83fa632dfc8384dfd8a21f4c4a39437521
KevinZheng99 <kevinzhengkz99@gmail.com>
2024-05-09 19:32:01 -0400

    progress on share modal

--- src/components/ShareModal/ShareModal.jsx
+++ src/components/ShareModal/ShareModal.jsx
@@ -1,9 +1,21 @@
+import { useContext } from "react";
+import Close from "../Icons/Close";
+import { ShareModalContext } from "../../store/share-modal";
+
 function ShareModal() {
+  const { isShowing } = useContext(ShareModalContext);
+
   return (
-    <div className="fixed p-10 rounded-xl w-1/4 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-200 z-20">
-      <div>
-        <span>Share</span>
-        <span></span>
+    <div
+      className={`fixed p-10 rounded-xl w-1/4 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-200 z-20 transition-all ${
+        isShowing ? "opacity-100" : "opacity-0"
+      }`}
+    >
+      <div className="flex items-center justify-between">
+        <span className="text-2xl font-medium">Share</span>
+        <span className="rounded-full cursor-pointer hover:bg-neutral-300 p-2 transition-all">
+          <Close />
+        </span>
       </div>
     </div>
   );
