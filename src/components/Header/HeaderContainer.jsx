84f099fc49a8055c43d678de0b351153e61c78b7
KevinZheng99 <kevinzhengkz99@gmail.com>
2024-05-12 22:56:42 -0400

    remove hamburger

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,5 +1,4 @@
 import HomeIcon from "./HomeIcon";
-import Hamburger from "./Hamburger";
 import Socials from "./Socials";
 
 function HeaderContainer() {
@@ -7,7 +6,6 @@ function HeaderContainer() {
     <header className="flex h-24 justify-between items-center sticky top-0 z-10 bg-black bg-opacity-70 xl:bg-transparent xl:bg-opacity-100">
       <HomeIcon />
       <Socials />
-      <Hamburger />
     </header>
   );
 }
